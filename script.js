c23ea1be7310b5d21b02b837c2cf7125198147e5
moezl <moez.mltifi@gmail.com>
2024-07-27 18:54:51 +0100

    create a generation images function

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-const api= "sk-proj-NTHUs6KEj9Kxa00pht9qT3BlbkFJwxp3DznM46IUEWONdm6w"
-console.log(api);
+const apiKey= "sk-proj-NTHUs6KEj9Kxa00pht9qT3BlbkFJwxp3DznM46IUEWONdm6w"
+const inp = document.getElementById("input")
+const images = document.querySelector(".images")
+
+const getImages = async () =>{
+    const methods = {
+    method: "POST",
+       headers:{ "Content-Type": "application/json",
+                 "Authorization":`Bearer ${apiKey}` 
+       },
+       body:JSON.stringify({
+        "prompt": inp.value,
+        "n": 3,
+        "size": "256x256"
+       })
+    }
+    const res = await fetch("https://api.openai.com/v1/images/generations",methods)
+    const data = await res.json()
+    const listImages = data.data
+    listImages.map(photo =>{
+        const container = document.createElement("div")
+        images.append(container)
+        const img = document.createElement("img")
+        container.append(img)
+        img.src= photo.url
+    })
+}
